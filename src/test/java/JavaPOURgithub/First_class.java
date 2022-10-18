689777c3ca51b9519c3f52b4fa62e49addeab943
cfr2454 <cafercolak2454@gmail.com>
2022-10-18 21:23:12 +0300

    CORRECTED OR FİXED THE WRONG DATA

--- src/test/java/JavaPOURgithub/First_class.java
+++ src/test/java/JavaPOURgithub/First_class.java
@@ -4,5 +4,7 @@ public class First_class {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("Git hub is a unique tool for collaboration");
+        System.out.println("New features of the application must be learned");
+        System.out.println("the last sentence");
     }
 }
