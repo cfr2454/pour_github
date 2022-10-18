72fd536f30b522dead8d1d1c3ee4bd8daebd2411
cfr2454 <cafercolak2454@gmail.com>
2022-10-18 20:52:50 +0300

    first setences

--- src/test/java/JavaPOURgithub/First_class.java
+++ src/test/java/JavaPOURgithub/First_class.java
@@ -3,5 +3,6 @@ package JavaPOURgithub;
 public class First_class {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("Git hub is a unique tool for collaboration");
     }
 }
